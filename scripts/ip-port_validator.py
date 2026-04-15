15eee4af418de5e245de4037c8c93a1052c83fe4
femboyenjoy <femboy@pusytroller.cf>
2026-04-15 12:42:05 +0800

    Update ip-port_validator.py

--- scripts/ip-port_validator.py
+++ scripts/ip-port_validator.py
@@ -12,7 +12,7 @@ THREADS = 100
 TIMEOUT = 5
 
 IP_API = "https://btapi.ipip.net/v2/trace"
-IP_TOKEN = "你的token"
+IP_TOKEN = "068f269ea236dc57215574f3542c8161e27fbf70"
 
 HTTP_FILE = "http_proxy.txt"
 HTTPS_FILE = "https_proxy.txt"
